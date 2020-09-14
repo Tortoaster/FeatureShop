abb23d7a84b98b0b05e80131933901b1ef640632
Rick van der Wal <rickvanderwalvis@gmail.com>
2020-09-14 15:30:58 +0200

    Klaar

--- src/Client.java
+++ src/Client.java
@@ -13,11 +13,119 @@ public class Client implements Runnable {
 
     private static final int WIDTH = 500, HEIGHT = 500, UI_HEIGHT = 30;
 
-    private JTextField ip;
-    private JTextField port;
-    private JTextField pass;
-    private JPanel messages;
-    private JPanel panel;
+    private final JTextField ip;
+    private final JTextField port;
+    private final JTextField pass;
+    private final JLabel status;
+    private final JPanel messages;
+
+    private final JFrame login = new JFrame();
+
+    {
+        JPanel panel = new JPanel();
+        {
+            panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
+            JPanel serverPanel = new JPanel();
+            {
+                ip = new JTextField("localhost", 15);
+                port = new JTextField("" + Server.PORT, 5);
+
+                serverPanel.add(new JLabel("IP: "));
+                serverPanel.add(ip);
+                serverPanel.add(new JLabel("Port: "));
+                serverPanel.add(port);
+            }
+
+            JPanel authPanel = new JPanel();
+            if (Conf.AUTH) {
+                pass = new JPasswordField(10);
+
+                serverPanel.add(new JLabel("Password: "));
+                serverPanel.add(pass);
+            }
+
+            JButton button = new JButton("Log in");
+            button.addActionListener(new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent actionEvent) {
+                    new Thread(Client.this).start();
+                }
+            });
+
+            status = new JLabel("Log in");
+
+            panel.add(status);
+            panel.add(serverPanel);
+            if (Conf.AUTH) panel.add(authPanel);
+            panel.add(button);
+        }
+
+        login.setContentPane(panel);
+        login.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        login.setLocationRelativeTo(null);
+        login.pack();
+        login.setVisible(true);
+    }
+
+    private final JFrame chat = new JFrame();
+
+    {
+        JPanel panel = new JPanel();
+        {
+            panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
+
+            messages = new JPanel();
+            messages.setPreferredSize(new Dimension(WIDTH, HEIGHT - UI_HEIGHT));
+
+            JPanel input = new JPanel();
+            {
+                JColorButton color = new JColorButton(Conf.COLOR_DEFAULT);
+                color.setPreferredSize(new Dimension(UI_HEIGHT, UI_HEIGHT));
+
+                JButton send = new JButton("Send");
+                send.setPreferredSize(new Dimension(100, UI_HEIGHT));
+
+                JTextField text = new JTextField();
+                text.setToolTipText("Say something...");
+                int width = WIDTH - send.getPreferredSize().width;
+                if (Conf.COLOR) width -= color.getPreferredSize().width;
+                text.setPreferredSize(new Dimension(width, UI_HEIGHT));
+
+                send.addActionListener(new ActionListener() {
+                    @Override
+                    public void actionPerformed(ActionEvent actionEvent) {
+                        Message message;
+                        if (Conf.COLOR) message = new Message(text.getText(), color.getSelectedColor());
+                        else message = new Message(text.getText(), Conf.COLOR_DEFAULT);
+
+                        if (Conf.CRYPTO) {
+                            message.encrypt(Conf.CRYPTO_FIRST_LAYER);
+                            message.encrypt(Conf.CRYPTO_SECOND_LAYER);
+                        }
+
+                        out.println(Server.CODE_MESSAGE);
+                        out.println(message.toString());
+                        text.setText("");
+                    }
+                });
+
+                if (Conf.COLOR) {
+                    input.add(color);
+                }
+
+                input.add(text);
+                input.add(send);
+            }
+
+            panel.add(messages);
+            panel.add(input);
+        }
+
+        chat.setContentPane(panel);
+        chat.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        chat.setLocationRelativeTo(null);
+        chat.pack();
+    }
 
     private Scanner in;
     private PrintWriter out;
@@ -29,119 +137,12 @@ public class Client implements Runnable {
     }
 
     public Client() {
-        if(Conf.LOG) {
+        if (Conf.LOG) {
             try {
                 log = new PrintWriter(new BufferedWriter(new FileWriter(Conf.LOG_CLIENT_PATH, true)), true);
             } catch (IOException e) {
                 e.printStackTrace();
             }
-        }
-
-        createUI();
-    }
-
-    private void createUI() {
-        JFrame frame = new JFrame();
-        {
-            panel = new JPanel(new CardLayout());
-            {
-                JPanel login = new JPanel();
-                {
-                    login.setLayout(new BoxLayout(login, BoxLayout.Y_AXIS));
-
-                    JPanel fields = new JPanel(new GridLayout(3, 2));
-                    {
-                        JLabel ipLabel = new JLabel("IP: ");
-                        ip = new JTextField("localhost");
-
-                        JLabel portLabel = new JLabel("Port: ");
-                        port = new JTextField("" + Server.PORT);
-
-                        fields.add(ipLabel);
-                        fields.add(ip);
-                        fields.add(portLabel);
-                        fields.add(port);
-
-                        if(Conf.AUTH) {
-                            JLabel passLabel = new JLabel("Password: ");
-                            pass = new JPasswordField();
-                            fields.add(passLabel);
-                            fields.add(pass);
-                        }
-                    }
-
-                    JButton button = new JButton("Log in");
-                    button.addActionListener(new ActionListener() {
-                        @Override
-                        public void actionPerformed(ActionEvent actionEvent) {
-                            new Thread(Client.this).start();
-                        }
-                    });
-
-                    login.add(fields);
-                    login.add(button);
-                }
-
-                JPanel chat = new JPanel();
-                {
-                    chat.setLayout(new BoxLayout(chat, BoxLayout.Y_AXIS));
-
-                    messages = new JPanel();
-                    messages.setPreferredSize(new Dimension(WIDTH, HEIGHT - UI_HEIGHT));
-
-                    JPanel input = new JPanel();
-                    {
-                        JColorButton color = new JColorButton(Conf.COLOR_DEFAULT);
-                        color.setPreferredSize(new Dimension(UI_HEIGHT, UI_HEIGHT));
-
-                        JButton send = new JButton("Send");
-                        send.setPreferredSize(new Dimension(100, UI_HEIGHT));
-
-                        JTextField text = new JTextField();
-                        text.setToolTipText("Say something...");
-                        int width = WIDTH - send.getPreferredSize().width;
-                        if(Conf.COLOR) width -= color.getPreferredSize().width;
-                        text.setPreferredSize(new Dimension(width, UI_HEIGHT));
-
-                        send.addActionListener(new ActionListener() {
-                            @Override
-                            public void actionPerformed(ActionEvent actionEvent) {
-                                Message message;
-                                if(Conf.COLOR) message = new Message(text.getText(), color.getSelectedColor());
-                                else message = new Message(text.getText(), Conf.COLOR_DEFAULT);
-
-                                if(Conf.CRYPTO) {
-                                    message.encrypt(Conf.CRYPTO_FIRST_LAYER);
-                                    message.encrypt(Conf.CRYPTO_SECOND_LAYER);
-                                }
-
-                                out.println(Server.CODE_MESSAGE);
-                                out.println(message.toString());
-                                text.setText("");
-                            }
-                        });
-
-                        if(Conf.COLOR) {
-                            input.add(color);
-                        }
-
-                        input.add(text);
-                        input.add(send);
-                    }
-
-                    chat.add(messages);
-                    chat.add(input);
-                }
-
-                panel.add(login);
-                panel.add(chat);
-            }
-
-            frame.setContentPane(panel);
-            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-            frame.setLocationRelativeTo(null);
-            frame.pack();
-            frame.setVisible(true);
         }
     }
 
@@ -155,9 +156,9 @@ public class Client implements Runnable {
             e.printStackTrace();
         }
 
-        boolean verified = false;
+        boolean verified;
 
-        if(Conf.AUTH) {
+        if (Conf.AUTH) {
             out.println(Server.CODE_LOGIN);
             out.println(pass.getText());
             verified = in.nextBoolean();
@@ -167,25 +168,28 @@ public class Client implements Runnable {
         }
 
         if (verified) {
-            ((CardLayout) panel.getLayout()).next(panel);
+            login.dispose();
+            chat.setVisible(true);
 
             while (in.hasNextLine()) {
                 Message message = Message.fromString(in.nextLine());
 
-                if(Conf.CRYPTO) {
+                if (Conf.CRYPTO) {
                     message.decrypt(Conf.CRYPTO_SECOND_LAYER);
                     message.decrypt(Conf.CRYPTO_FIRST_LAYER);
                 }
 
-                if(Conf.LOG) log.println(message);
+                if (Conf.LOG) log.println(message);
 
                 JLabel label = new JLabel(message.getSender() + " " + message.getContent());
                 label.setForeground(message.getColor());
-                label.setBackground(Color.WHITE);
                 messages.add(label);
                 messages.validate();
                 messages.repaint();
             }
+        } else {
+            status.setText("Wrong password");
+            status.setForeground(Color.RED);
         }
 
         in.close();
--- src/Conf.java
+++ src/Conf.java
@@ -4,7 +4,7 @@ public class Conf {
     public static final boolean COLOR = true;
     public static final Color COLOR_DEFAULT = Color.BLACK;
 
-    public static final boolean LOG = true;
+    public static final boolean LOG = false;
     public static final String LOG_SERVER_PATH = "logs/server/log.txt";
     public static final String LOG_CLIENT_PATH = "logs/client/log.txt";
 
@@ -12,6 +12,6 @@ public class Conf {
     public static final Cipher CRYPTO_FIRST_LAYER = Cipher.ROT13;
     public static final Cipher CRYPTO_SECOND_LAYER = Cipher.REVERSE;
 
-    public static final boolean AUTH = true;
+    public static final boolean AUTH = false;
     public static final String AUTH_PASS = "12345";
 }
